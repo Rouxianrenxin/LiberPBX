23f0e423b0b725102e3658635e9396a727374766
aspurlock <aspurlock@iwsit.com>
2019-02-21 16:10:10 -0600

    Initial commit - with auth:make, mysql hotfix, and node_modules

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+
+        $this->mapRestRoutes();
+
+        //
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Map Rest Routes... I don't want to use passport for auth, i'd rather just use laravel's default auth for internal rest calls
+     */
+    protected function mapRestRoutes()
+    {
+        Route::prefix('rest')
+            ->middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/rest.php'));
+    }
+}
---
+++ routes/rest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aspurlock
+ * Date: 2/21/2019
+ * Time: 4:03 PM
+ */
